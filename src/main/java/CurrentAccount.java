31b76723e5ed37a1b3d6c1f9b39b0e30e1a68fd8
u18202374-Chaks <u18202374@tuks.co.za>
2022-05-11 02:16:45 +0200

    CurrentAccount function withdraw coded

--- src/main/java/CurrentAccount.java
+++ src/main/java/CurrentAccount.java
@@ -22,6 +22,39 @@ public class CurrentAccount{
     }
 
 
+    public BigDecimal withdraw(Integer accnum,BigDecimal amountowithdraw) throws AccountNotFoundException, WithdrawalAmountTooLargeException
+    {
+        //check if the account number exist in the database
+        if(accounnum == accnum)
+        {
+            BigDecimal overwithdrawal;
+            int result ;
+
+            overwithdrawal = balance.add(overdraft);
+
+            result = overwithdrawal.compareTo(amountowithdraw);
+
+            if(result==1)
+            {
+
+                overwithdrawal = overwithdrawal.subtract(amountowithdraw);
+                return overwithdrawal;
+            }
+            else
+            {
+                throw new WithdrawalAmountTooLargeException("The withdrawal amount is greater than the balance");
+            }
+        }
+        else
+        {
+            throw new AccountNotFoundException("Account number was not found in the database");
+        }
+
+
+
+    }
+
+
 
 
 }
